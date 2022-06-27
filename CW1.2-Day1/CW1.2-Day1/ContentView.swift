dbbcd4594309f1b788a44e259fa048209e9f8cc7
Batool Ali <batoolaliismail@gmail.com>
2022-06-27 16:01:41 +0300

    cw1.2
    cw1.2

--- CW1.2-Day1/CW1.2-Day1/ContentView.swift
+++ CW1.2-Day1/CW1.2-Day1/ContentView.swift
@@ -10,16 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.blue, .white],
-                                 startPoint: .top,
-                                 endPoint: .center)
-        Text("Hello, world!")
-            .padding()
-        }}
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Image("Bg")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    Image(systemName: "cube.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                    Spacer()
+                    Text("العاصمة")
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal)
+                        .font(.system(size: 30))
+                    Spacer()
+                    Image(systemName: "gearshape.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                    
+                    
+                }
+                HStack{
+                    Text("3:43")
+                        .font(.system(size: 90, weight: .bold , design: .rounded))
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+                
+            }.padding()}
+        }
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
     }
-}
+
